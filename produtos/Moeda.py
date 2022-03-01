bb6edb3be9e06a12ace6bda67d59cecc35e14567
Caio Pra <caioprsilva@gmail.com>
2022-03-01 11:45:39 -0300

    Produto and Moeda classes basic structure created

---
+++ produtos/Moeda.py
@@ -0,0 +1,9 @@
+class Moeda():
+    def __init__(self, cotacao, sigla, nome, horario_atualizacao):
+        self.cotacao = cotacao
+        self.sigla = sigla
+        self.nome = nome
+        self.horario_atualizacao = horario_atualizacao
+
+    def atualizar_cotacao(self, sigla):
+        pass
---
+++ produtos/Produto.py
@@ -0,0 +1,11 @@
+class Produto():
+    def __init__(self, valorCompra, moeda, taxaLucro, valorVenda, qtdDisponivel, disponivel):
+        self.valorCompra = valorCompra
+        self.moeda = moeda
+        self.taxaLucro = taxaLucro
+        self.valorVenda = valorVenda
+        self.qtdDisponivel = qtdDisponivel
+        self.disponivel = disponivel
+    
+    def atualizar_valor(self, valorCompra, taxaLucro, moeda):
+        pass
