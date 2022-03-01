3e85cf6e19eadeb81f8f3f036ed2802d073a4949
Caio Pra <caioprsilva@gmail.com>
2022-03-01 00:00:40 -0300

    Basic structures of Cliente and Endereco classes

---
+++ clientes/Endereco.py
@@ -0,0 +1,11 @@
+class Endereco():
+    def __init__(self, logradouro, bairro, cidade, cep, estado, complemento):
+        self.logradouro = logradouro
+        self.bairro = bairro
+        self.cidade = cidade
+        self.cep = cep
+        self.estado = estado
+        self.complemento = complemento
+
+    def atualizarEndereco(self):
+        pass
---
+++ produtos/Moeda.py
@@ -0,0 +1,23 @@
+class Cliente():
+    def __init__(self, nomeCliente="", idCliente=0, cpfCliente="", nascimentoCliente="", emailCliente="", senhaCliente="", telefoneCliente="", enderecoCliente=""):
+
+        self.nomeCliente = nomeCliente
+        self.idCliente = idCliente
+        self.cpfCliente = cpfCliente
+        self.nascimentoCliente = nascimentoCliente
+        self.emailCliente = emailCliente
+        self.senhaCliente = senhaCliente
+        self.telefoneCliente = telefoneCliente
+        self.enderecoCliente = enderecoCliente
+
+    def cadastrarCliente(self):
+        pass
+
+    def atualizarCliente(self):
+        pass
+
+    def deletarCliente(self):
+        pass
+
+    def login(self):  # usar com email e senha
+        pass
