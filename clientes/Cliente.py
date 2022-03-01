490a2a96e03d39d6f8a84a16430c8718861961b9
Caio Pra <caioprsilva@gmail.com>
2022-03-01 12:14:58 -0300

    bancoFuncionarios created to store Funcionarios objects; attributes names changed at Cliente; Funcionario class structured

--- clientes/Cliente.py
+++ clientes/Cliente.py
@@ -1,14 +1,16 @@
-class Cliente():
-    def __init__(self, nomeCliente="", idCliente=0, cpfCliente="", nascimentoCliente="", emailCliente="", senhaCliente="", telefoneCliente="", enderecoCliente=""):
+from Endereco import Endereco
 
-        self.nomeCliente = nomeCliente
+class Cliente():
+    def __init__(self, nome="", idCliente=0, cpf="", nascimento="", email="", senha="", telefone="", logradouro="", bairro="", cidade="", cep="", estado="", complemento="", numero=""):
+
+        self.nome = nome
         self.idCliente = idCliente
-        self.cpfCliente = cpfCliente
-        self.nascimentoCliente = nascimentoCliente
-        self.emailCliente = emailCliente
-        self.senhaCliente = senhaCliente
-        self.telefoneCliente = telefoneCliente
-        self.enderecoCliente = enderecoCliente
+        self.cpf = cpf
+        self.nascimento = nascimento
+        self.email = email
+        self.senha = senha
+        self.telefone = telefone
+        self.endereco = Endereco(logradouro, bairro, cidade, cep, estado, complemento, numero)
 
     def cadastrarCliente(self):
         pass
---
+++ clientes/Funcionario.py
@@ -0,0 +1,16 @@
+class Funcionario():
+    def __init__(self, nome, id, cpf, email, senha, admin, nascimento, telefone):
+        self.nome = nome
+        self.id = id
+        self.cpf = cpf
+        self.email = email
+        self.senha = senha
+        self.admin = admin
+        self.nascimento = nascimento
+        self.telefone = telefone
+    
+    def atualizarDados(self):
+        pass
+
+    def login(self, email, senha):
+        pass
