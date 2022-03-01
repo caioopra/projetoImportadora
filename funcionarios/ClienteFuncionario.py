f97634b253e74d957a682001609b8ab8f5f2bcb5
Caio Pra <caioprsilva@gmail.com>
2022-03-01 17:05:52 -0300

    ClienteFuncionario remodeled, no longer uses heritance

--- funcionarios/ClienteFuncionario.py
+++ funcionarios/ClienteFuncionario.py
@@ -2,10 +2,9 @@ from Funcionario import Funcionario
 from clientes.Endereco import Endereco
 
 
-class ClienteFuncionario(Funcionario):
-    def __init__(self, nome, id, cpf, email, senha, admin, nascimento, telefone, desconto, logradouro, bairro, cidade, cep, estado, complemento, numero):
-        Funcionario.__init__(self, nome, id, cpf, email,
-                             senha, admin, nascimento, telefone)
+class ClienteFuncionario():
+    def __init__(self, funcionario, desconto, logradouro, bairro, cidade, cep, estado, complemento, numero):
+        self.funcionario = funcionario
         self.desconto = desconto
         self.endereco = Endereco(
             logradouro, bairro, cidade, cep, estado, complemento, numero)
